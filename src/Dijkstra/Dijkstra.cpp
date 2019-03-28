5866d48ac951904d53848199510a008c1d61e4c2
DarlanJurak <darlan.jurak@gmail.com>
2019-03-28 16:32:50 -0300

    removed BFS legacy

--- src/Dijkstra/Dijkstra.cpp
+++ src/Dijkstra/Dijkstra.cpp
@@ -2,29 +2,3 @@
 #include <queue>
 
 #include "../SquareGrid/SquareGrid.hpp"
-
-std::unordered_map<Pos, Pos>
-BreadthFirstSearch(SquareGrid grid, Pos start, Pos goal) {
-  std::queue<Pos> frontier;
-  frontier.push(start);
-
-  std::unordered_map<Pos, Pos> came_from;
-  came_from[start] = start;
-
-  while (!frontier.empty()) {
-    Pos current = frontier.front();
-    frontier.pop();
-
-    if (current == goal){
-      break;
-    }
-
-    for (Pos next : grid.Neighbors(current)) {
-      if (came_from.find(next) == came_from.end()) {
-        frontier.push(next);
-        came_from[next] = current;
-      }
-    }
-  }
-  return came_from;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,10 @@
 #include "SquareGrid/SquareGrid.hpp"
 #include "Dijkstra/Dijkstra.cpp"
 
-void old_test_BFS();
-    
 int main(int argc, char** argv) {
 
-    
+
 
     return 0;
-
-}
-
-void old_test_BFS(){
-
-    SquareGrid grid = make_diagram1();
-
-    Pos start;
-    start.x = 2;
-    start.y = 2;
-
-    Pos goal;
-    start.x = 17;
-    start.y = 11;
-    
-    auto parents = BreadthFirstSearch(grid, start, goal);
-    draw_grid(grid, 2, nullptr, &parents);
 
 }
