ce7c8bfdd0c77c33cf7af0429c0686201de4b4c9
DarlanJurak <darlan.jurak@gmail.com>
2019-03-29 09:40:13 -0300

    commented Dijkstra algorithm

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
  all:
 	g++ src/main.cpp -o bin/test -std=c++17 
 	clear
-	./bin/test 0 0 1 9
+	./bin/test 0 0 7 8
--- src/Dijkstra/Dijkstra.cpp
+++ src/Dijkstra/Dijkstra.cpp
@@ -5,6 +5,12 @@
 
 template<typename Pos> std::vector<Pos> reconstruct_path( Pos start, Pos goal, std::unordered_map<Pos, Pos> came_from );
 
+
+/*
+*
+* Description: Dijkstra (lol)
+*
+*/
 template<typename Pos, typename GridWithWeights>
 void Dijkstra_Search
   (GridWithWeights graph,
@@ -13,25 +19,27 @@ void Dijkstra_Search
    std::unordered_map<Pos, Pos>& came_from,
    std::unordered_map<Pos, double>& cost_so_far)
 {
-  PriorityQueue<Pos, double> frontier;
-  frontier.put(start, 0);
+  PriorityQueue<Pos, double> frontier;                                      // Ordering frontier exploration by lowest cost
+  frontier.put(start, 0);                                                   // Frontier exploration starts on start point
 
-  came_from[start] = start;
-  cost_so_far[start] = 0;
+  came_from[start]    = start;
+  cost_so_far[start]  = 0;
   
-  while (!frontier.empty()) {
-    Pos current = frontier.get();
+  // Exploration by lowest cost
+  while (!frontier.empty()) { 
 
-    if (current == goal) {
+    Pos current = frontier.get();                                           // Gets lowest costy position on frontier
+
+    if (current == goal) {                                                  // Early exit when reached the goal
       break;
     }
 
-    for (Pos next : graph.Neighbors(current)) {
-      double new_cost = cost_so_far[current] + graph.cost(current, next);
-      if (cost_so_far.find(next) == cost_so_far.end()
-          || new_cost < cost_so_far[next]) {
+    for (Pos next : graph.Neighbors(current)) {                             // Frontier expansion
+      double new_cost = cost_so_far[current] + graph.cost(current, next);   // Add information about the cost of new frontier's position cost
+      if (cost_so_far.find(next) == cost_so_far.end()                       // Position not considered yet
+          || new_cost < cost_so_far[next]) {                                // During a expansion comming from other path, the cost can be lower, so we updated it
         cost_so_far[next] = new_cost;
-        came_from[next] = current;
+        came_from[next]   = current;
         frontier.put(next, new_cost);
       }
     }
@@ -40,9 +48,10 @@ void Dijkstra_Search
 
 template<typename Pos>
 std::vector<Pos> reconstruct_path(
-   Pos start, Pos goal,
-   std::unordered_map<Pos, Pos> came_from
-) {
+  Pos start, Pos goal,
+  std::unordered_map<Pos, Pos> came_from)
+{
+     
   std::vector<Pos> path;
   Pos current = goal;
   while (current != start) {
@@ -52,4 +61,5 @@ std::vector<Pos> reconstruct_path(
   path.push_back(start); // optional
   std::reverse(path.begin(), path.end());
   return path;
+  
 }
